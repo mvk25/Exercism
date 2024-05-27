4562ab322ab2d5b132124dfacafeca45f9d1b98e
mvk25 <lizhawai@gmail.com>
2024-05-27 20:58:37 +0300

    Diffie Hellman(Modular Power)

---
+++ rust/diffie-hellman/src/main.rs
@@ -0,0 +1,35 @@
+use rand::Rng;
+
+pub fn private_key(p: u64) -> u64 {
+    rand::thread_rng().gen_range(2..p)
+}
+
+fn modular_exponentiation(base: u128, exp: u64, modular: u64) -> u64 {
+    let mut e = exp;
+    let mut b = base;
+    let mut result = 1;
+    while e > 0 {
+        if e % 2 == 1 {
+            result = (result * b) % modular as u128;
+        }
+        println!("b: {} e: {} result: {}", b, e, result);
+        b = (b * b) % modular as u128;
+        e = e / 2;
+    }
+
+    result as u64
+}
+
+pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+    modular_exponentiation(g as u128, a, p)
+}
+
+pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+    modular_exponentiation(b_pub as u128, a, p)
+}
+
+fn main() {
+    // let res: u64 = modular_exponentiation(8, 4294967296, 4294967299);
+    let res2: u64 = modular_exponentiation(2, 1982, 19);
+    println!("{}", res2);
+}
